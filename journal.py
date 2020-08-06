522685b28eeb01e858ae04fce6d2fae5af9a1664
janthenelli <57114460+janthenelli@users.noreply.github.com>
2020-08-06 13:09:36 -0400

    added macros.html file for render_field, created register template, created routes for register, login, logout

--- journal.py
+++ journal.py
@@ -37,6 +37,47 @@ def after_request():
     g.db.close()
     return response
 
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        try:
+            models.User.create_user(
+                username=form.username.data,
+                email=form.email.data,
+                password=form.password.data
+            )
+        except ValueError:
+            flash("User with '{}' email already exists.".format(form.email.data), "error")
+        else:
+            flash("User '{}' created successfully!".format(form.email.data), "success")
+            return redirect(url_for('index'))
+    return render_template('register.html' form=form)
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.email == form.email.data)
+        except DoesNotExist:
+            flash("Email or password does not match.", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
+                flash("Welcome, {}!".format(user.username), "success")
+                return redirect(url_for('index'))
+            else:
+                flash("Email or password does not match.", "error")
+    return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out.", "success")
+    return redirect(url_for('index'))
+
 @app.route('/')
 @app.route('/entries')
 def index():
